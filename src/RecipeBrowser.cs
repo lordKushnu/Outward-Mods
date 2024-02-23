d698d66c8761b54ced1bf5b2f64a5ec48c24a196
lordKushnu <fartlaser@gmail.com>
2024-02-22 22:10:45 -0600

    fix file structure

---
+++ src/RecipeBrowser.cs
@@ -0,0 +1,79 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using RecipeBrowser.Services;
+using System;
+
+// RENAME 'OutwardModTemplate' TO SOMETHING ELSE
+namespace RecipeBrowser
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    public class RecipeBrowser : BaseUnityPlugin
+    {
+        // Choose a GUID for your project. Change "myname" and "mymod".
+        public const string GUID = "theGrungler.RecipeBrowser";
+        // Choose a NAME for your project, generally the same as your Assembly Name.
+        public const string NAME = "RecipeBrowser";
+        // Increment the VERSION when you release a new version of your mod.
+        public const string VERSION = "1.0.0";
+
+        public const string DISPLAY_NAME = "Recipe Browser";
+
+        // For accessing your BepInEx Logger from outside of this class (eg Plugin.Log.LogMessage("");)
+        internal static ManualLogSource Log;
+
+        private static Harmony _harmony;
+
+        private static InventoryStashService inventoryStashService;
+        // If you need settings, define them like so:
+        public static ConfigEntry<bool> ShowUnlearnedRecipies;
+        // Awake is called when your plugin is created. Use this to set up your mod.
+        internal void Awake()
+        {
+            Log = this.Logger;
+            Log.LogMessage($"Starting {NAME} {VERSION}!");
+            InitializeConfig();
+
+            // Any config settings you define should be set up like this:
+
+            // Harmony is for patching methods. If you're not patching anything, you can comment-out or delete this line.
+            _harmony = new Harmony(GUID);
+            _harmony.PatchAll();
+            inventoryStashService = new InventoryStashService();
+        }
+
+        internal void OnDestroy()
+        {
+            _harmony.UnpatchSelf();
+            inventoryStashService.deleteStash();
+        }
+
+        private void InitializeConfig()
+        {
+            ShowUnlearnedRecipies = Config.Bind(DISPLAY_NAME, "Show Unlearned Recipies", false, "This allows the Recipie Browser to show Unlearned Recipies");
+        }
+
+        // Update is called once per frame. Use this only if needed.
+        // You also have all other MonoBehaviour methods available (OnGUI, etc)
+        internal void Update()
+        {
+            //Log.LogMessage("This is an update");
+        }
+
+
+        // This is an example of a Harmony patch.
+        // If you're not using this, you should delete it.
+        [HarmonyPatch(typeof(ResourcesPrefabManager), nameof(ResourcesPrefabManager.Load))]
+        public class ResourcesPrefabManager_Load
+        {
+            static void Postfix()
+            {
+                // This is a "Postfix" (runs after original) on ResourcesPrefabManager.Load
+                // For more documentation on Harmony, see the Harmony Wiki.
+                // https://harmony.pardeike.net/
+            }
+        }
+
+    }
+}
---
+++ src/Services/InventoryStashService.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static RecipeBrowser.Patches.InventoryPatches;
+using UnityEngine;
+using RecipeBrowser.Patches;
+
+namespace RecipeBrowser.Services
+{
+    internal class InventoryStashService
+    {
+        public enum StashDisplayTypes
+        {
+            Disabled,
+            Inventory,
+            Equipment,
+            Merchant
+        }
+        private Character _character;
+        private CharacterUI _characterUI => _character.CharacterUI;
+        private const string _inventoryDisplayPath = "Canvas/GameplayPanels/Menus/CharacterMenus/MainPanel/Content/MiddlePanel/Inventory/Content/SectionContent";
+        private ContainerDisplay storedStashDisplay;
+        private int timesCalled = 0;
+
+        public int Start()
+        {
+            timesCalled++;
+            return timesCalled;
+        }
+
+        public void createStashMenu(CharacterUI characterUI, CharacterUI.MenuScreens menu, Item item)
+        {
+            if (menu != CharacterUI.MenuScreens.Inventory && menu != CharacterUI.MenuScreens.Equipment
+                && menu != CharacterUI.MenuScreens.Shop)
+                return;
+
+            ToggleStashes(characterUI);
+        }
+        private void ToggleStashes(CharacterUI _instance)
+        {
+            var inventoryContentDisplay = _instance.transform.Find(_inventoryDisplayPath).GetComponent<InventoryContentDisplay>();
+            AddStashDisplay(_instance, inventoryContentDisplay);
+        }
+
+        private void AddStashDisplay(CharacterUI _instance, InventoryContentDisplay inventoryContentDisplay)
+        {
+            var inventoryPath = inventoryContentDisplay.transform.GetGameObjectPath();
+            if (inventoryPath.EndsWith(_inventoryDisplayPath))
+            {
+                //It's an Inventory
+                if(storedStashDisplay == null)
+                {
+                    RecipeBrowser.Log.LogInfo($"creating new stash display");
+                    RectTransform parentTransform = inventoryContentDisplay.m_overrideContentHolder;
+                    if (parentTransform == null)
+                        parentTransform = inventoryContentDisplay.m_inventoriesScrollRect?.content;
+                    var containerDisplayPrefab = inventoryContentDisplay.ContainerDisplayPrefab;
+                    ContainerDisplay stashDisplay = UnityEngine.Object.Instantiate(containerDisplayPrefab).GetComponent<ContainerDisplay>();
+                    stashDisplay.transform.SetParent(parentTransform);
+                    stashDisplay.transform.ResetLocal();
+                    stashDisplay.name = "HostStashDisplay";
+                    stashDisplay.SetName("Host Display");
+
+                    var header = stashDisplay.transform.Find("Header");
+                    if(header != null)
+                    {
+                        var lblWeight = (RectTransform)header.transform.Find("lblWeight");
+                        if(lblWeight != null)
+                        {
+                            UnityEngine.Object.Destroy(lblWeight.gameObject);
+                        }
+                        var iconWeight = header.transform.Find("iconWeight");
+                        if(iconWeight != null)
+                        {
+                            UnityEngine.Object.Destroy(iconWeight.gameObject);
+                        }
+                    }
+                    storedStashDisplay = stashDisplay;
+                    ShowStashPanel(_instance);
+                }
+                else
+                {
+                    RecipeBrowser.Log.LogInfo($"Stash Display already exists");
+                    ShowStashPanel(_instance);
+                }
+            }
+
+            else
+            {
+                RecipeBrowser.Log.LogInfo($"not an Inventory?");
+            }
+        }
+
+        private void ShowStashPanel(CharacterUI instance)
+        {
+            var charHost = CharacterManager.Instance.GetWorldHostCharacter();
+            var localCharacter = CharacterManager.Instance.GetFirstLocalCharacter();
+            var localStash = localCharacter.Stash;
+            ItemContainer hostStash = charHost.Stash;
+            var localStashPanel = localCharacter.CharacterUI.StashPanel;
+            localStashPanel.SetStash(hostStash);
+            hostStash.ShowContent(localCharacter);
+            storedStashDisplay.SetReferencedContainer(hostStash);
+        }
+        public void deleteStash()
+        {
+            storedStashDisplay.gameObject.SetActive(false);
+            GameObject.Destroy(storedStashDisplay.gameObject);
+        }
+    }
+}
